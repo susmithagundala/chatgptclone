1bc6751b5c3371411b8ff5fb152034f695266818
Gundala Susmitha <166511796+susmithagundala@users.noreply.github.com>
2024-09-04 12:02:04 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chat from './Chat';
+
+function App() {
+  return (
+    <div className="chat-container">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
